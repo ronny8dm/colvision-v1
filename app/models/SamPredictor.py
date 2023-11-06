6f65345a4c447318fb0bb8f5da5267182d49cdb9
Ronny Diaz <ronnydiaz23@gmail.com>
2023-11-06 13:36:35 +0000

    added method to render segmentation

--- app/models/SamPredictor.py
+++ app/models/SamPredictor.py
@@ -1,9 +1,11 @@
 import os
+
+import numpy as np
 import torch
 import logging
 import cv2
+import supervision as sv
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
-
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -18,7 +20,6 @@ class SamPredictor:
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.checkpoint_path = os.path.join(os.path.dirname(__file__), "home", "weights", "sam_vit_h_4b8939.pth")
         self.model_type = "vit_h"
-        self.output_path = os.path.join(home, 'static', 'masked_image.jpg')
 
         # Check if the checkpoint file exists
         if not os.path.isfile(self.checkpoint_path):
@@ -26,20 +27,57 @@ class SamPredictor:
 
         logger.debug(f"Using device {self.device}")
 
+    def generate_annotated_image(self, sam_result, image_bgr, output_path):
+        mask_annotator = sv.MaskAnnotator(color_lookup=sv.ColorLookup.INDEX)
+        detections = sv.Detections.from_sam(sam_result=sam_result)
+        annotated_image = mask_annotator.annotate(scene=image_bgr.copy(), detections=detections)
+
+        # Save the annotated image
+        sv.plot_images_grid(
+            images=[image_bgr, annotated_image],
+            grid_size=(1, 2),
+            titles=[output_path, annotated_image]
+        )
+
     def predict(self):
+        # Load the SAM model and generate predictions
         sam = sam_model_registry[self.model_type](checkpoint=self.checkpoint_path).to(device=self.device)
         mask_generator = SamAutomaticMaskGenerator(sam)
-
-        image_path = os.path.join(os.path.dirname(__file__), "home", "assets", "living-room-1.jpg")  # Add your image extension
+        image_path = os.path.join(os.path.dirname(__file__), "home", "assets", "living-room-1.jpg")   # Corrected path using self.home
 
         # Check if the image file exists
         if not os.path.isfile(image_path):
             raise FileNotFoundError(f"Image file not found at {image_path}")
 
+        # Read and process the image
         image_bgr = cv2.imread(image_path)
         image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+
+        # Generate masks using SAM
         sam_result = mask_generator.generate(image_rgb)
 
-        masked_image = image_rgb * sam_result[0]['segmentation'][None, :, :, None]
-        cv2.imwrite(self.output_path, masked_image)
-        return self.output_path
+        # Check and convert the result to a serializable format if necessary
+        if isinstance(sam_result, list) and all(isinstance(item, dict) for item in sam_result):
+            # Convert any ndarray items to lists for JSON serialization
+            for mask in sam_result:
+                for key, value in mask.items():
+                    if isinstance(value, np.ndarray):
+                        mask[key] = value.tolist()  # Convert the numpy array to a list
+
+            # Here you would generate the annotated image, but you need to decide where to save it
+            # For example, let's assume you have a 'static' directory in 'self.home' to store images
+            output_path = os.path.join(self.home, "static", "annotated_image.jpg")
+            self.generate_annotated_image(sam_result, image_bgr, output_path)
+
+            return sam_result
+        else:
+            logging.error("The result from the mask generator is not in the expected format.")
+            return None
+
+    def get_image(self):
+        image_path = os.path.join(self.home, "assets", "living-room-1.jpg")  # or whatever your image path is
+        if not os.path.isfile(image_path):
+            raise FileNotFoundError(f"Image file not found at {image_path}")
+        image_bgr = cv2.imread(image_path)
+        return image_bgr
+
--- app/routes/predictor_route.py
+++ app/routes/predictor_route.py
@@ -1,17 +1,33 @@
+from flask import jsonify, url_for
 from app import app
-from flask import render_template
 import os
 from app.models.SamPredictor import SamPredictor
+import logging
 
 
 @app.route('/predict')
 def predict():
-    # Project directory path
-    home = os.path.dirname(
-        os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # This will point to the root of your project
+    home = os.path.abspath(os.path.join(__file__, "../../.."))
 
     predictor = SamPredictor(home)
-    result = predictor.predict()
-    image_path = predictor.predict()
+    masks = predictor.predict()
 
-    return render_template('render.html', image_path='static/masked_image.jpg')
+    logging.debug(f"Type of masks: {type(masks)}")
+    logging.debug(f"Content of masks: {masks}")
+
+    if isinstance(masks, list):
+        # Generate the annotated image
+        static_dir = os.path.join(app.root_path, 'static')
+        filename = 'annotated_image.jpg'
+        output_path = os.path.join(static_dir, filename)
+
+        # Obtain the original image (you will need to implement this)
+        image_bgr = predictor.get_image()
+
+        # Save the annotated image using the new method
+        predictor.generate_annotated_image(masks, image_bgr, output_path)
+
+        # Return the URL for the generated image
+        return jsonify({"annotated_image_url": url_for('static', filename=filename)})
+    else:
+        return jsonify({"error": "Predictor did not return a list of dictionaries"}), 400
